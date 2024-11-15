b6a51643bac2a3ba6069caf2e02322cf91bb1828
sara lopez <saralefay2010@gmail.com>
2024-11-15 00:48:47 -0500

    Se crean perfil de contratante, se necesita validar el navbar menu para que funcione el "perfil"

--- Orquesta.Shared/Entities/Representante.cs
+++ Orquesta.Shared/Entities/Representante.cs
@@ -35,5 +35,10 @@ namespace Orquesta.Shared.Entities
 
         [JsonIgnore]
         public ICollection<Agrupacion> AgrupacionSolistas { get; set; } = new List<Agrupacion>();
+        [JsonIgnore]
+        public ICollection<Calificacion_Contratante> Calificaciones_Contratante { get; } = new List<Calificacion_Contratante>();
+
+        [JsonIgnore]
+        public ICollection<Calificacion_Agrupacion> Calificaciones_Agrupacion { get; } = new List<Calificacion_Agrupacion>();
     }
 }
